4927f14677af971225b3b555eb97da02ebd8e112
Pawel Zoladek <pawel.zoladek@intive.com>
2020-05-13 18:35:30 +0200

    Third commit (last in lab4)

--- app/src/main/java/com/example/padtm_lab4/MainActivity.java
+++ app/src/main/java/com/example/padtm_lab4/MainActivity.java
@@ -47,4 +47,16 @@ public class MainActivity extends AppCompatActivity {
         Intent intencja = new Intent(this, DodajWpis.class);
         startActivityForResult(intencja, 1);
     }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            String nowy = (String) extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
 }
