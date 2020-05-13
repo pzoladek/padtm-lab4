215df0810557b4d806e72bda8d782a18e8629d7a
Pawel Zoladek <pawel.zoladek@intive.com>
2020-05-13 18:25:44 +0200

    First commit

---
+++ app/src/main/java/com/example/padtm_lab4/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.padtm_lab4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<String> target;
+    private ArrayAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] values = new String[] { "Pies",
+                "Kot", "Koń", "Gołąb", "Kruk", "Dzik", "Karp",
+                "Osioł", "Chomik", "Mysz", "Jeż", "Kraluch" };
+
+        this.target = new ArrayList<String>();
+        this.target.addAll(Arrays.asList(values));
+        this.adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, this.target);
+
+        ListView listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(this.adapter);
+    }
+}
